c4c6cea3f581bb92a7599d31073e09159c2cfffd
thiareMohamed <thiaremohamed.mt@gmail.com>
2022-09-12 01:31:34 -0700

    Write a C program that prints exacty with proper grammer, but the outcome is a piece of art, fllowed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * return:Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper gammar, but the outcome is a piece of art,\n");
+	return (0);
+}
